9bb8f669a0888c0017208d2f2f63585dd125f7f3
vy3191 <venkyafhs@gmail.com>
2019-12-08 13:34:08 -0500

    Added get commetns by ID method

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,8 @@ const router = express.Router({mergeParams: true });
 
 router.get("/", async (req,res) => {
    try {
-    const comments = await db.findPostComments(req.params.id);    
+    const comments = await db.findPostComments(req.params.id);
+    
     if(comments.length === 0) res.status(404).json({ message: "The post with the specified ID does not exist." });
     res.status(200).json(comments);   
    } catch {
@@ -12,6 +13,17 @@ router.get("/", async (req,res) => {
    }
   
 });
+
+router.get("/:commentId", async (req,res) => {
+    try {
+      const comment = await db.findCommentById(req.params.commentId);
+      console.log(comment);
+      if(comment.length ===0) res.status(404).json({message:"The post with the speciifed ID comment does not exist."});
+      res.status(200).json(comment)
+    } catch {
+      res.status(500).json({error:"The comments information could not be retrieved."});
+    }
+})
 
 
 module.exports = router;
