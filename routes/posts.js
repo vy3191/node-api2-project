252690e832a7ce65311a58a90f14565aff03f318
vy3191 <venkyafhs@gmail.com>
2019-12-07 23:09:29 -0500

    Added get post by Id

--- routes/posts.js
+++ routes/posts.js
@@ -14,5 +14,15 @@ router.get("/", async (req,res) => {
    }
 });
 
+router.get("/:id", async (req,res) => {
+   try {
+     const data = await db.findById(req.params.id);
+     const user = data[0];
+     if(!user.title) res.status(404).json({ message: `The post with the specified POST-ID ${id} does not exist.`});
+     res.status(200).json(user);
+   }catch(err) {
+     res.status(500).json({error: `The post information with id ${req.params.id} could not be retrieved.` });
+   }
+});
 
 module.exports = router;
