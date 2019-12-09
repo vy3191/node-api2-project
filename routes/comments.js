d302871b91ea1ae3fec0b35adabf3824329388b9
vy3191 <venkyafhs@gmail.com>
2019-12-08 21:13:03 -0500

    Added adding comments t existing comments

--- routes/comments.js
+++ routes/comments.js
@@ -2,6 +2,29 @@ const express = require("express");
 const db = require("../data/db");
 const router = express.Router({mergeParams: true });
 
+router.post("/",  (req,res) => {  
+      const { id } = req.params;
+      db.findCommentById(id)
+        .then(data => {
+            return res.json(data)
+        })
+        .then(data => db.insertComment(req.body.text))
+        .then(data => res.json(data))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the comment to the database" }))
+     
+
+      // if (!req.body.text) res.status(400).json({ errorMessage: "Please provide text for the comment." });
+      // db.insertComment(req.body.text)
+      //   .then( data => {
+      //      if(!data) res.status(404).json({ message: "The post with the specified ID does not exist." });
+      //      res.status(201).json(data);
+      //   })
+      //   .catch(err => {
+      //     res.status(500).json({ error: "There was an error while saving the comment to the database" });
+
+      //   });
+});
+
 router.get("/", async (req,res) => {
    try {
     const comments = await db.findPostComments(req.params.id);
